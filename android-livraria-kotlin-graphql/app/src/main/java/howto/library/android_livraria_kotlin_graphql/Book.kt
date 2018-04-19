49a5477b84304685c06b10d4d4153db3ad2d1e46
Helena Strada <h.strada@hotmail.com>
2018-04-19 17:01:04 -0300

    ListView

---
+++ android-livraria-kotlin-graphql/app/src/main/java/howto/library/android_livraria_kotlin_graphql/Book.kt
@@ -0,0 +1,6 @@
+package howto.library.android_livraria_kotlin_graphql
+
+/**
+ * Created by Helena Strada on 19/04/2018.
+ */
+data class Book (var name: String, var description: String)
--- android-livraria-kotlin-graphql/app/src/main/java/howto/library/android_livraria_kotlin_graphql/MainActivity.kt
+++ android-livraria-kotlin-graphql/app/src/main/java/howto/library/android_livraria_kotlin_graphql/MainActivity.kt
@@ -1,12 +1,15 @@
 package howto.library.android_livraria_kotlin_graphql
 
+import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
+import android.view.*
+import android.widget.BaseAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
+import android.widget.TextView
 import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.Response
@@ -29,9 +32,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val livrosLista: MutableList<Book> = mutableListOf()
+
         client = setupApollo()
 
-        client.query(BooksQuery.builder().build())
+        client.query(BooksQuery
+                .builder()
+                .build())
                 .enqueue(object : ApolloCall.Callback<BooksQuery.Data>() {
 
                     override fun onFailure(e: ApolloException) {
@@ -39,38 +46,77 @@ class MainActivity : AppCompatActivity() {
                     }
 
                     override fun onResponse(response: Response<BooksQuery.Data>) {
-                        Log.info(" " + response.data()?.allBooks())
+
+                        response.data()?.allBooks()?.forEachIndexed { index, element ->
+                            livrosLista.add(Book(response.data()?.allBooks()?.get(index)?.name().toString()
+                                    , response.data()?.allBooks()?.get(index)?.description().toString()))
+                        }
+
                     }
 
                 })
-
-//        val okHttpClient = OkHttpClient.Builder().build()
-//
-//        val apolloClient = ApolloClient
-//                .builder()
-//                .serverUrl("http://10.0.3.2:8080/")
-//                .okHttpClient(okHttpClient)
-//                .build();
-//
-//        apolloClient.query(BooksQuery.builder().build()).enqueue(
-//                object : ApolloCall.Callback<BooksQuery.Data> () {
-//                    override fun onResponse(response: Response<BooksQuery.Data>) {
-//                        val books = response.data()?.allBooks
-//                        Log.d("sucesso", response.data()?.allBooks().toString())
-//                    }
-//
-//                    override fun onFailure(e: ApolloException) {
-//                        Log.d("falha", e.message.toString())
-//                    }
-//
-//                }
-//        )
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+        val lv = findViewById<ListView>(R.id.lvBooks)
+        lv.adapter = ListBooksAdapter(livrosLista,this)
+
     }
+
+    private class ListBooksAdapter(books: List<Book>, context: Context) : BaseAdapter() {
+
+        var sListBooks = books
+
+        private val mInflator: LayoutInflater
+
+        init {
+            this.mInflator = LayoutInflater.from(context)
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View? {
+            val view: View?
+            val vh: ListRowHolder
+            if (p1 == null) {
+                view = this.mInflator.inflate(R.layout.list_item_book, p2, false)
+                vh = ListRowHolder(view)
+                view.tag = vh
+            } else {
+                view = p1
+                vh = view.tag as ListRowHolder
+            }
+
+            vh.nomeLivro.text = sListBooks[p0].name
+            vh.descricaoLivro.text = sListBooks[p0].description
+            return view
+        }
+
+        override fun getItem(p0: Int): Any {
+            return sListBooks[p0]
+        }
+
+        override fun getCount(): Int {
+            return sListBooks.size
+        }
+
+    }
+
+    private class ListRowHolder(row: View?) {
+        public val nomeLivro: TextView
+        public val descricaoLivro: TextView
+
+        init {
+            this.nomeLivro = row?.findViewById<TextView>(R.id.tvNomeLivro) as TextView
+            this.descricaoLivro = row?.findViewById<TextView>(R.id.tvDescricaoLivro) as TextView
+        }
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
